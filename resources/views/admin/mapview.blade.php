6f5d2189180fda03c965807d8e72ffee06eddca9
Sierra <55088939+evelynsierra@users.noreply.github.com>
2022-06-26 22:06:40 +0700

    succeed map visualization

--- resources/views/admin/mapview.blade.php
+++ resources/views/admin/mapview.blade.php
@@ -56,9 +56,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- /.content-header -->
 
             <!-- For Map-->
-            <div class="chart">
-                <canvas id="myChart" width="400" height="200"></canvas>
-            </div>
+            @include('earthquake')
             <!-- !End Map -->
 
             <!-- Main content -->
@@ -153,53 +151,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="{{asset('lte/dist/js/adminlte.min.js')}}"></script>
     
     <!-- Map -->
-    <script>
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'line', // also try bar or other graph types
-
-            // The data for our dataset
-            data: {
-                labels: ["Jun 2016", "Jul 2016", "Aug 2016", "Sep 2016", "Oct 2016", "Nov 2016", "Dec 2016", "Jan 2017", "Feb 2017", "Mar 2017", "Apr 2017", "May 2017"],
-                // Information about the dataset
-                datasets: [{
-                    label: "Rainfall",
-                    backgroundColor: 'lightblue',
-                    borderColor: 'royalblue',
-                    data: [26.4, 39.8, 66.8, 66.4, 40.6, 55.2, 77.4, 69.8, 57.8, 76, 110.8, 142.6],
-                }]
-            },
-
-            // Configuration options
-            options: {
-                layout: {
-                    padding: 10,
-                },
-                legend: {
-                    position: 'bottom',
-                },
-                title: {
-                    display: true,
-                    text: 'Precipitation in Toronto'
-                },
-                scales: {
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Precipitation in mm'
-                        }
-                    }],
-                    xAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Month of the Year'
-                        }
-                    }]
-                }
-            }
-        });
-    </script>
 
 </body>
 
